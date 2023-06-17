9fc3fce8b1df676eb00ea57acb6c679e2b9d61d1
jiwoo <han_jiwoo@snu.ac.kr>
2023-06-17 16:48:44 +0900

    [feat] envfile

--- src/apis/axios.js
+++ src/apis/axios.js
@@ -57,3 +57,12 @@ instanceWithToken.interceptors.response.use(
     return Promise.reject(error);
   }
 );
+
+// src/apis/axios.js
+
+if (process.env.NODE_ENV === "development") {
+  axios.defaults.baseURL = "http://localhost:8000/api";
+} else {
+  axios.defaults.baseURL =
+    "https://port-0-snulion-week12-koh2xlisex7sv.sel4.cloudtype.app/api";
+}
